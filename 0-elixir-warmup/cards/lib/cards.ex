292d4112337b4241400d7f0f87162948f7732da3
Rachid Boufous <rachidboufous32@gmail.com>
2024-05-03 09:23:53 +0100

    Creating card module
    - cards modules

---
+++ 0-elixir-warmup/cards/lib/cards.ex
@@ -0,0 +1,25 @@
+defmodule Cards do
+
+
+  def hello do
+    "hi there" # we don't have to write the return keyword as elixir return the last object on function scope
+  end
+
+
+  def create_deck do
+
+    # creating a list of elements
+
+    ["ACE", "TWO", "THREE"]
+
+  end
+
+
+  def shuffle_deck(deck) do
+
+
+
+  end
+
+
+end
